db37ec6570187696e68e1d923434710fa32d19d7
fermibot <ashwinuoh@gmail.com>
2020-06-17 20:25:12 -0500

    geohash_unittest.py: added extra unittests to see if the geohash objects are retaining functionality after the initial access of their methods

--- geohash_unittest.py
+++ geohash_unittest.py
@@ -4,29 +4,34 @@ from geohash import Geohash
 
 class GeohashUnittest(unittest.TestCase):
 
-    def test_something(self):
+    def test_all_geohash_use_cases(self):
         print()
         for hash_kind in [32, 64, 256, 4096]:
             print(f"Testing: {hash_kind}ghs")
-            geo_hash = Geohash(hash_type=hash_kind)
-            lat_long_pair = [-5.6034, 42.6057]
-            calculated_hash = geo_hash.nums_to_geohash(lat_long_pair)
-            calculated_nums = [round(x, 4) for x in geo_hash.hash_to_nums(calculated_hash, split_type=2)]
+            geohash_inst = Geohash(hash_type=hash_kind)
 
+            lat_long_pair = [-5.6034, 42.6057]
+            calculated_hash = geohash_inst.nums_to_geohash(lat_long_pair)
+            calculated_nums = [round(x, 4) for x in geohash_inst.hash_to_nums(calculated_hash, split_type=2)]
+            self.assertEqual(lat_long_pair, calculated_nums)
+
+            lat_long_pair = [-85.7826, 65.4975]
+            calculated_hash = geohash_inst.nums_to_geohash(lat_long_pair)
+            calculated_nums = [round(x, 4) for x in geohash_inst.hash_to_nums(calculated_hash, split_type=2)]
             self.assertEqual(lat_long_pair, calculated_nums)
 
             if hash_kind in [256, 4096]:
                 print(f"\tTesting quartets for: {hash_kind}ghs")
                 quartet = [-5.6034, 42.6057, 4.8153, -0.6849]
-                calculated_hash = geo_hash.nums_to_geohash(quartet)
-                calculated_nums = [round(x, 4) for x in geo_hash.hash_to_nums(calculated_hash, split_type=4)]
+                calculated_hash = geohash_inst.nums_to_geohash(quartet)
+                calculated_nums = [round(x, 4) for x in geohash_inst.hash_to_nums(calculated_hash, split_type=4)]
                 self.assertEqual(quartet, calculated_nums)
                 if hash_kind == 4096:
                     print(f"\t\tTesting sextets for: {hash_kind}ghs")
                     sextet = [-5.6034, 42.6057, 4.8153, -0.6849, 5.9462, 54.6548]
-                    calculated_hash = geo_hash.nums_to_geohash(sextet)
+                    calculated_hash = geohash_inst.nums_to_geohash(sextet)
                     calculated_nums = [round(x, 4) for x in
-                                       geo_hash.hash_to_nums(calculated_hash, split_type=6)]
+                                       geohash_inst.hash_to_nums(calculated_hash, split_type=6)]
                     self.assertEqual(sextet, calculated_nums)
 
 
